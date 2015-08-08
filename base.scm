660b198f4ba8185a437d6eac462188d4a1aa2725
Pepijn Verlaan <pepijnverlaan@yahoo.com>
2015-08-08 21:37:48 +0200

    Improve the base library a bit
    Ditch the use of apply in favor of just passing the arguments to the arithmetic
    helper functions as a list. It's less code this way.

--- base.scm
+++ base.scm
@@ -1,54 +1,50 @@
-(define (tail-recursive-sum acc . vals)
+(define (tail-recursive-sum acc vals)
   (if (null? vals)
       acc
-      (apply
-        tail-recursive-sum
-        (cons (binary-sum acc (car vals))
-              (cdr vals)))))
+      (tail-recursive-sum
+        (binary-sum acc (car vals))
+        (cdr vals))))
 
 (define (+ . vals)
-  (apply tail-recursive-sum (cons 0 vals)))
+  (tail-recursive-sum 0 vals))
 
-(define (tail-recursive-subtract acc . vals)
+(define (tail-recursive-subtract acc vals)
   (if (null? vals)
       acc
-      (apply
-        tail-recursive-subtract
-        (cons (binary-subtract acc (car vals))
-              (cdr vals)))))
+      (tail-recursive-subtract
+        (binary-subtract acc (car vals))
+        (cdr vals))))
 
 (define (- . vals)
   (if (null? vals)
-      0
+      0 ;should raise an error, but I don't have them yet
       (if (null? (cdr vals))
           (binary-subtract 0 (car vals))
-          (apply tail-recursive-subtract vals))))
+          (tail-recursive-subtract (car vals) (cdr vals)))))
 
-(define (tail-recursive-multiply acc . vals)
+(define (tail-recursive-multiply acc vals)
   (if (null? vals)
       acc
-      (apply
-        tail-recursive-multiply
-        (cons (binary-multiply acc (car vals))
-              (cdr vals)))))
+      (tail-recursive-multiply
+        (binary-multiply acc (car vals))
+        (cdr vals))))
 
 (define (* . vals)
-  (apply tail-recursive-multiply (cons 1 vals)))
+  (tail-recursive-multiply 1 vals))
 
-(define (tail-recursive-divide acc . vals)
+(define (tail-recursive-divide acc vals)
   (if (null? vals)
     acc
-    (apply
-      tail-recursive-divide
-      (cons (binary-divide acc (car vals))
-            (cdr vals)))))
+    (tail-recursive-divide
+      (binary-divide acc (car vals))
+      (cdr vals))))
 
 (define (/ . vals)
   (if (null? vals)
-    0
+    0 ;should raise an error, but I don't have them yet
     (if (null? (cdr vals))
         (binary-divide 1 (car vals))
-        (apply tail-recursive-subtract vals))))
+        (tail-recursive-divide (car vals) (cdr vals)))))
 
 (define call/cc call-with-current-continuation)
 
